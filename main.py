746e103f314a500f31fbc87f01b1754d2ff589ad
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2025-12-09 17:21:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+1-misol
+yosh = int(input("Yoshingizni kiriting: "))
+
+if yosh >= 18:
+    print("Kattasiz!")
+
+#2-misol
+raqam = int(input("raqam kiriting: "))
+
+if raqam <= 10:
+    print("10 dan kichik!")
+elif raqam == 10:
+    print("10 ga yeng!")
+
+#3-misol
+a = input("kiriting: ")
+
+if a == "hello":
+    print("salom!")
+elif a == "bye":
+    print("xayr!")
+
+#4-misol
+yil = input("Yilni aniqlang: ")
+
+if yil == "Kabisat":
+    print("Kabisat yili!")
+else:
+    print("oddiy yil!")
+
+#5-misol
+raqam = int(input("raqam kiriting:"))
+
+if raqam == "0":
+    print("0")
+elif raqam == "1":
+    print("1")
+else:
+    print('Boshqa raqam!')
